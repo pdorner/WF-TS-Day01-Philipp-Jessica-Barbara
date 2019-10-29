89429849117796815a4a8a71dbe2d29dede9f8ba
Philipp Dorner <pdorner@gmx.net>
2019-10-29 14:15:53 +0100

    change

--- Advanced/js/basic.js
+++ Advanced/js/basic.js
@@ -1,15 +1,53 @@
-var something = [];
-var object = { a: false, b: true, c: true };
+// let something = []
+// let object = { a: true, b: true, c: true }
 // for (let key in object) {
-//   something.push(() => console.log(key))
+//   // this time push a string, instead of an anonymous function
+//   something.push("the key is: " + key)
 // }
-function what() {
-    var _loop_1 = function (whatever) {
-        var sum = 2 + 2;
-        something.push(function () { return console.log(sum); });
-    };
-    for (var whatever in object) {
-        _loop_1(whatever);
-    }
+// var w : any = function(){
+// 	console.log ("Hello");
+// }
+// for (let whatever in something) {
+//   something.push(w);
+// }
+// something.forEach(str => w());
+// console.log(something);
+// something[3].w();
+//create CupCakes with values for name, price, img
+var winter = {
+    name: "winter-cupcake",
+    price: "5.99",
+    img: "./img/food5.jpg"
+};
+var halloween = {
+    name: "halloween-cupcake",
+    price: "6.99",
+    img: "./img/food7.jpg"
+};
+var unicorn = {
+    name: "unicorn-cupcake",
+    price: "6.99",
+    img: "./img/food2.jpg"
+};
+var gold = {
+    name: "gold-cupcake",
+    price: "25000.99",
+    img: "./img/food4.png"
+};
+var pink = {
+    name: "pink-cupcake",
+    price: "3.99",
+    img: "./img/food3.jpg"
+};
+var sugar = {
+    name: "sugarcorn-cupcake",
+    price: "6.99",
+    img: "./img/food6.jpg"
+};
+//creat in loop accordion 
+var menu = [winter, halloween, unicorn, gold, pink, sugar];
+for (var i = 0; i < menu.length; i++) {
+    $(".accordion").append("<div class=\"card\">\n                            <div class=\"card-header\" id=\"heading" + +i + "\">\n                                <h2 class=\"mb-0\">\n                                    <button style = \"width :90%\" class=\"btn btn-link\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapse" + +i + "\" aria-expanded=\"false\" aria-controls=\"collapse" + +i + "\">\n                                        <div class =\"row\"><h3 class=\"col-7 offset-1\">" + menu[i].name + "</h3>  \n\t       \t\t\t\t\t\t\t\t<h3 class=\"col-3 text-right\">" + menu[i].price + " \u20AC</h3></div>\n                                    </button>\n                                </h2>\n                            </div>\n                            <div id=\"collapse" + +i + "\" class=\"collapse\" aria-labelledby=\"heading" + +i + "\" data-parent=\"#accordionExample\">\n                                <div class=\"card-body\">\n                                \t<img class=\"card-img\" src=\"" + menu[i].img + "\">\n                                \t<input></input>\n                                </div>\n                            </div>\n                        </div>");
 }
-something.forEach(function (item) { return what(); });
+// winter.getName();
+// winter.getPrice();
--- Advanced/js/basic.ts
+++ Advanced/js/basic.ts
@@ -1,15 +1,88 @@
-let something = []
-let object = { a: false, b: true, c: true }
+// let something = []
+// let object = { a: true, b: true, c: true }
 
 // for (let key in object) {
-//   something.push(() => console.log(key))
+//   // this time push a string, instead of an anonymous function
+//   something.push("the key is: " + key)
 // }
-function what(){
 
-for (let whatever in object) {
-		let sum = 2+2;
-  something.push(() => console.log(sum))
+
+// var w : any = function(){
+// 	console.log ("Hello");
+// }
+// for (let whatever in something) {
+
+//   something.push(w);
+// }
+
+// something.forEach(str => w());
+// console.log(something);
+
+// something[3].w();
+
+
+//create CupCakes with values for name, price, img
+let winter = {
+   name: "winter-cupcake",
+   price: "5.99",
+   img: "./img/food5.jpg",
+   
+};
+var halloween = {
+    name: "halloween-cupcake",
+    price: "6.99",
+    img: "./img/food7.jpg",
+
+     
+};
+var unicorn = {
+    name: "unicorn-cupcake",
+    price: "6.99",
+    img: "./img/food2.jpg",
+
+     
+};var gold = {
+    name: "gold-cupcake",
+    price: "25000.99",
+    img: "./img/food4.png",
+
+     
+};var pink = {
+    name: "pink-cupcake",
+    price: "3.99",
+    img: "./img/food3.jpg",
+
+     
+};var sugar = {
+    name: "sugarcorn-cupcake",
+    price: "6.99",
+    img: "./img/food6.jpg",
+
+     
+};
+
+//creat in loop accordion 
+var menu : Array<any>=[winter, halloween, unicorn, gold, pink, sugar];
+for(let i = 0; i<menu.length;i++){
+	       $(".accordion").append(`<div class="card">
+                            <div class="card-header" id="heading${+i}">
+                                <h2 class="mb-0">
+                                    <button style = "width :90%" class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${+i}" aria-expanded="false" aria-controls="collapse${+i}">
+                                        <div class ="row"><h3 class="col-7 offset-1">${menu[i].name}</h3>  
+	       								<h3 class="col-3 text-right">${menu[i].price} €</h3></div>
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapse${+i}" class="collapse" aria-labelledby="heading${+i}" data-parent="#accordionExample">
+                                <div class="card-body">
+                                	<img class="card-img" src="${menu[i].img}">
+                                	<input></input>
+                                </div>
+                            </div>
+                        </div>`);
 }
-}
-something.forEach(item => what());
+
+
+// winter.getName();
+// winter.getPrice();
 
